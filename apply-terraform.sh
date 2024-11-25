51c137245be4e8be756938d353237debf65f5655
vbuvana <vbuvaneswaree@gmail.com>
2024-11-25 15:10:23 +0100

    Terraform destroy

--- apply-terraform.sh
+++ apply-terraform.sh
@@ -3,6 +3,6 @@
 set -eu #if any problems in code the whole process will be stopped
 terraform init
 #command to apply
-terraform apply -auto-approve
+#terraform apply -auto-approve
 #command to destroy
-#terraform destroy -auto-approve
+terraform destroy -auto-approve
